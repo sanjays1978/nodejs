e4054896fbce42db39be6ff68fc0cc7d84f6e881
Sanjay Sharma <sanjays1978@gmail.com>
2022-09-08 14:47:24 +0530

    Mongo Connection
    app.js to show how to connect to mongodb

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const port = 3300;
+const app = express();
+const mongourl = 'mongodb://127.0.0.1:27017/'
+
+MongoClient.connect(mongourl, (err, client) => {
+    if(err) throw err;
+    app.listen(port,() => {
+        console.log('Server running on port ${port}');
+    })
+})
+
